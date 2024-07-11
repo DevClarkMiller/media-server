f9a1af576ed35ecd1be62811524f5ce1ad56ed33
Clark Miller <63131937+MeatBallFreak69@users.noreply.github.com>
2024-07-10 22:28:19 -0400

    Added two .gitignores, also updated some packages

--- media-server-front-end/src/components/FileTile.js
+++ media-server-front-end/src/components/FileTile.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useMemo } from "react";
 
 //Components
 import BoxWrapper from "../mill-comps/components/BoxWrapper";
@@ -6,8 +6,7 @@ import BoxWrapper from "../mill-comps/components/BoxWrapper";
 //Icons
 import { IoCloudDownloadOutline, IoCloudDownload  } from "react-icons/io5";
 
-const FileTile = ({file}) =>{
-    const [displayName, setDisplayName] = useState("");
+const FileTile = ({checkOpacity, file}) =>{
     const [hovering, setHovering] = useState(false);
     const [textClass, setTextClass] = useState("");
     const [btnClass, setBtnClass] = useState("");
@@ -15,23 +14,9 @@ const FileTile = ({file}) =>{
     const textRef = useRef();
     const btnRef = useRef();
 
-    useEffect(() =>{
-        if(file){
-            setDisplayName(
-                (file.og_name.length < 25) ? file.og_name.length : file.og_name.substring(0, 25) + "..."
-            );
-        }
-    }, [file]);
-
-    const checkOpacity = (element, setClass) =>{
-        const computedStyle = window.getComputedStyle(element);
-        const opacityValue = computedStyle.getPropertyValue('opacity');
-        if(opacityValue === "0"){
-            setClass("max-h-0 max-w-0 select-none");
-        }else if(opacityValue === "1"){
-            setClass("");
-        }
-    }
+    const displayName = useMemo(() =>(
+        (file?.og_name?.length < 25) ? file?.og_name : file?.og_name.substring(0, 25) + "..."
+    ), [file]);
 
     //Adds listeners to the transitioned property for the text and button when the component renders
     useEffect(() =>{
@@ -54,11 +39,11 @@ const FileTile = ({file}) =>{
 
 
     return(
-        <div onMouseEnter={() => setHovering(true)} onMouseLeave={() => setHovering(false)}>
+        <div onClick={() => setHovering(true)} onMouseLeave={() => setHovering(false)}>
             <BoxWrapper className="flex items-center justify-center w-64 h-10 fileTile!bg-appleGray shadow-md !p-2 text-center">
-                <p ref={textRef} className={`tran-trans-opac duration-300 ${!hovering&& "!max-h-max !max-w-max"} ${hovering && "opacity-0"} ${textClass}`}>{displayName}</p>
+                <p ref={textRef} className={`tran-trans-opac duration-300 hover:cursor-pointer ${!hovering&& "!max-h-max !max-w-max"} ${hovering && "opacity-0"} ${textClass}`}>{displayName}</p>
                 <div ref={btnRef}>
-                    <a href="https://clarkmiller.ca/publicFiles/resume.pdf" target="_blank" download={file.og_name}>
+                    <a href="https://clarkmiller.ca/publicFiles/resume.pdf"  download={file.og_name}>
                         <IoCloudDownloadOutline className={`tran-trans-opac duration-300 hover:cursor-pointer hover:text-appleLightBlue ${!hovering && "opacity-0 "} ${btnClass}`} />
                         </a>
                     </div>
--- media-server-front-end/src/components/UserFiles.js
+++ media-server-front-end/src/components/UserFiles.js
@@ -10,11 +10,22 @@ import FileTile from "./FileTile";
 const UserFiles = () =>{
     const {renderedFiles, setRendererdFiles} = useContext(FileContext);
 
+    //Func is placed up here for limited rendering
+    const checkOpacity = (element, setClass) =>{
+        const computedStyle = window.getComputedStyle(element);
+        const opacityValue = computedStyle.getPropertyValue('opacity');
+        if(opacityValue === "0"){
+            setClass("max-h-0 max-w-0  select-none");
+        }else if(opacityValue === "1"){
+            setClass("");
+        }
+    }
+
     return(
         <div className="userFiles flex flex-wrap justify-center">
             {renderedFiles&&
                 renderedFiles.map((file) =>(
-                    <FileTile key={file.og_name} file={file}/>
+                    <FileTile checkOpacity={checkOpacity} key={file.og_name} file={file}/>
                 ))
             }
         </div>
